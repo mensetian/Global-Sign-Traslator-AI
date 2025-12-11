67eaaee787a9adede7612949dfa6329e07a0f449
mensetian <35746008+mensetian@users.noreply.github.com>
2025-12-11 13:40:22 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,16 @@
-
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    // Si cargamos las variables aquí, están listas para ser inyectadas por Vite
-    const env = loadEnv(mode, '.', ''); 
-    
-    return {
-
-        base: './', 
-        
-        server: {
-            port: 3000,
-            host: '0.0.0.0',
-        },
-        plugins: [react()],
-
-        
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, '.'),
-            }
-        }
-    };
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: './', // Vital para que cargue en Vercel
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  };
 });
